bc80aefedf07bc2ac0f6450fd5e2db537bf4f809
neven.shalata <nevenh.1994@gmail.com>
2022-03-20 11:06:28 +0200

    first try to push project

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function data(){
+    let firstname=document.getElementById("firstname");
+    let lastname=document.getElementById("lastname");
+    let age=document.getElementById("age");
+
+    alert(firstname.value+ " " + lastname.value+ " " + "age:"+ age.value);
+}
